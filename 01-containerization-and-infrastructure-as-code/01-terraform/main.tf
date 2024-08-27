b6c43ff166367d636c1b6c3fa78adea7c160c0ab
siriyaparn <siriyasriboonma@gmail.com>
2024-08-27 18:50:46 +0700

    initial commit

---
+++ 01-containerization-and-infrastructure-as-code/01-terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+#  credentials = 
+  project = "<Your Project ID>"
+  region  = "asia-southeast1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "<Your Unique Bucket Name>"
+  location      = "ASIA"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
