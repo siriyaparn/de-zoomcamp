c65986f115dc2d5a47a9a2b6e510369b7b0878ce
siriyaparn <siriyasriboonma@gmail.com>
2025-01-19 21:51:38 +0700

    initial commit

---
+++ 01-containerization-and-infrastructure-as-code/docker/pipeline.py
@@ -0,0 +1,8 @@
+import sys
+import pandas as pd
+
+print(sys.argv)
+day = sys.argv[1]  # the first argument passed to the script after the script name
+
+# some funcy stuff with pandas
+print(f"Job finished successfully for day {day}")
